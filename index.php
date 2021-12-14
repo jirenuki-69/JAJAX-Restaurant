074aa090107a63f06671b12ee38ed8fa68cf514d
Miguel Fuentes <mfuentesmodelo@gmail.com>
2021-12-13 22:43:15 -0600

    Configs

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php
+include 'templates/header.php';
+?>
+
+<body>
+
+  Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sint quam sapiente laboriosam magnam officiis. Praesentium harum dolorum laudantium inventore libero debitis perferendis natus! Voluptatibus nesciunt corporis, illo doloremque officiis provident!
+
+  <?php
+  include 'templates/footer.php';
+  ?>
+
+</body>
+
+</html>
---
+++ templates/footer.php
@@ -0,0 +1,5 @@
+<footer>
+  <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.min.js" integrity="sha384-VHvPCCyXqtD5DqJeNxl2dtTyhF78xXNXdkwX1CZeRusQfRKp+tA7hAShOK/B/fQ2" crossorigin="anonymous"></script>
+  <script src="assets/scripts.js"></script>
+</footer>
