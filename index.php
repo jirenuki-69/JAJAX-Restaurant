0fbcdc49c610e50f6031a2f2b24088143dc544e7
Miguel Fuentes <mfuentesmodelo@gmail.com>
2021-12-14 17:17:38 -0600

    test

--- index.php
+++ index.php
@@ -6,7 +6,6 @@ include 'templates/header.php';
 
 <body>
 
-  Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sint quam sapiente laboriosam magnam officiis. Praesentium harum dolorum laudantium inventore libero debitis perferendis natus! Voluptatibus nesciunt corporis, illo doloremque officiis provident!
 
   <?php
   include 'templates/footer.php';
