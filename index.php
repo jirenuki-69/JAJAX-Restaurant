ebc1e20e1ad043ab7fd2b9402d5160cc401ab677
Miguel Fuentes <mfuentesmodelo@gmail.com>
2021-12-15 23:18:27 -0600

    Index Part 2

--- index.php
+++ index.php
@@ -2,29 +2,34 @@
 <html lang="en">
 
 <head>
-  <?php
-  include 'templates/header.php';
-  ?>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="css/index.css">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
   <title>JAJAX Restaurants</title>
 </head>
 
 <body>
 
   <div class="root-container">
-    <div class="banner"></div>
+    <div class="banner">
+      <div class="content-title">
+        <p>Conoce Los Mejores Restaurantes<br />del Mundo</p>
+        <p class="subtitle">Comida Exquisita y de Primer Nivel, Hecha por los Mejores Chefs del Mundo</p>
+        <ul class="content-nav">
+          <li style="margin-right: 20px;"><a href="pages/catalogo.php">Catálogo</a></li>
+          <li><a href="pages/detalles.php" style="color: #FFFFFF;">Detalles (Temporal)</a></li>
+        </ul>
+      </div>
+    </div>
   </div>
-  <nav class="navbar navbar-dark bg-dark">
-    <a href="pages/catalogo.php" class="nav-link" style="color: #FFFFFF;">Catálogo</a>
-  </nav>
-
-  <nav class="navbar navbar-dark bg-dark">
-    <a href="pages/detalles.php" class="nav-link" style="color: #FFFFFF;">Detalles (Temporal)</a>
-  </nav>
-
-
-  <?php
-  include 'templates/footer.php';
-  ?>
+  <footer>
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <script src="js/script.js"></script>
+  </footer>
 
 </body>
 
