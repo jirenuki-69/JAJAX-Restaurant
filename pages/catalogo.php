12dcf965688aa415fd812a6c9a895b818576b2b3
Miguel Fuentes <mfuentesmodelo@gmail.com>
2021-12-17 13:07:43 -0600

    Add & Edit Buttons

--- pages/catalogo.php
+++ pages/catalogo.php
@@ -7,14 +7,25 @@
   ?>
   <link rel="stylesheet" href="../css/catalogo.css">
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500;600;700&display=swap" rel="stylesheet">
   <title>Catálogo</title>
 </head>
 
 <body>
   <div class="catalog-container container">
-    <div class="search-box">
-      <button class="btn-search"><i class="fas fa-search"></i></button>
-      <input id="catalog-search" type="text" class="input-search" placeholder="Type to Search...">
+    <div class="catalog-header">
+      <div class="search-box">
+        <button class="btn-search"><i class="fas fa-search"></i></button>
+        <input id="catalog-search" type="text" class="input-search" placeholder="Type to Search...">
+      </div>
+      <button id="btn-agregar" type="button" class="custom-button">
+        <span class="button-text">Agregar</span>
+        <span class="button-icon">
+          <ion-icon name="add-outline"></ion-icon>
+        </span>
+      </button>
     </div>
     <div class="cards-container">
       <div class="my-card">
@@ -24,6 +35,7 @@
           <p class="card-desc">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequuntur nam illum perspiciatis magni sunt quo, assumenda ducimus obcaecati? Vero, a soluta? Itaque fugiat minus cum exercitationem laboriosam, quae ipsum repellat?</p>
           <div class="card-buttons">
             <button id="btn-more-info" class="btn btn-primary">Detalles</button>
+            <button id="btn-edit" class="btn btn-success">Editar</button>
             <button id="btn-delete" class="btn btn-danger">Eliminar</button>
           </div>
         </div>
@@ -35,6 +47,7 @@
           <p class="card-desc">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequuntur nam illum perspiciatis magni sunt quo, assumenda ducimus obcaecati? Vero, a soluta? Itaque fugiat minus cum exercitationem laboriosam, quae ipsum repellat?</p>
           <div class="card-buttons">
             <button id="btn-more-info" class="btn btn-primary">Detalles</button>
+            <button id="btn-edit" class="btn btn-success">Editar</button>
             <button id="btn-delete" class="btn btn-danger">Eliminar</button>
           </div>
         </div>
@@ -46,6 +59,7 @@
           <p class="card-desc">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequuntur nam illum perspiciatis magni sunt quo, assumenda ducimus obcaecati? Vero, a soluta? Itaque fugiat minus cum exercitationem laboriosam, quae ipsum repellat?</p>
           <div class="card-buttons">
             <button id="btn-more-info" class="btn btn-primary">Detalles</button>
+            <button id="btn-edit" class="btn btn-success">Editar</button>
             <button id="btn-delete" class="btn btn-danger">Eliminar</button>
           </div>
         </div>
@@ -57,6 +71,7 @@
           <p class="card-desc">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequuntur nam illum perspiciatis magni sunt quo, assumenda ducimus obcaecati? Vero, a soluta? Itaque fugiat minus cum exercitationem laboriosam, quae ipsum repellat?</p>
           <div class="card-buttons">
             <button id="btn-more-info" class="btn btn-primary">Detalles</button>
+            <button id="btn-edit" class="btn btn-success">Editar</button>
             <button id="btn-delete" class="btn btn-danger">Eliminar</button>
           </div>
         </div>
@@ -68,6 +83,7 @@
           <p class="card-desc">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequuntur nam illum perspiciatis magni sunt quo, assumenda ducimus obcaecati? Vero, a soluta? Itaque fugiat minus cum exercitationem laboriosam, quae ipsum repellat?</p>
           <div class="card-buttons">
             <button id="btn-more-info" class="btn btn-primary">Detalles</button>
+            <button id="btn-edit" class="btn btn-success">Editar</button>
             <button id="btn-delete" class="btn btn-danger">Eliminar</button>
           </div>
         </div>
@@ -82,6 +98,7 @@
         <p class="card-desc">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequuntur nam illum perspiciatis magni sunt quo, assumenda ducimus obcaecati? Vero, a soluta? Itaque fugiat minus cum exercitationem laboriosam, quae ipsum repellat?</p>
         <div class="card-buttons">
           <button id="btn-more-info" class="btn btn-primary">Detalles</button>
+          <button id="btn-edit" class="btn btn-success">Editar</button>
           <button id="btn-delete" class="btn btn-danger">Eliminar</button>
         </div>
       </div>
@@ -92,5 +109,7 @@
 <?php
 include '../templates/footer.php';
 ?>
+<script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+<script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
 
 </html>
