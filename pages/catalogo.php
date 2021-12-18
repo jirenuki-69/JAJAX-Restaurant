e898ce73396272f3cae091b9e7ba35109692bd08
Sergio mendoza <sergio_mdza@outlook.com>
2021-12-17 18:19:59 -0600

    Avances

--- pages/catalogo.php
+++ pages/catalogo.php
@@ -4,6 +4,7 @@
 <head>
   <?php
   include '../templates/header.php';
+  include "../utils/connection.php";
   ?>
   <link rel="stylesheet" href="../css/catalogo.css">
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
@@ -12,8 +13,13 @@
   <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500;600;700&display=swap" rel="stylesheet">
   <title>Catálogo</title>
 </head>
-
 <body>
+  <?php
+    $obj =  new connection();
+    $conn = $obj->connection_db();
+    $sql = "SELECT * FROM restaurantes ";
+    $result = $conn->query($sql);      
+  ?>
   <div class="catalog-container container">
     <div class="catalog-header">
       <div class="search-box">
@@ -27,8 +33,31 @@
         </span>
       </button>
     </div>
+
     <div class="cards-container">
+      <?php 
+        while($row = $result->fetch_assoc()) {
+            echo '<script>';
+            echo 'console.log(Row:'. json_encode( $row ) .')';
+            echo '</script>';
+      ?>
       <div class="my-card">
+        <img class="card-img" src="<?php echo $row["image"] ?>" alt="Image">
+        <div class="card-info">
+          <p class="card-title"><?php echo $row["name"] ?></p>
+          <p class="card-desc"><?php echo $row["description"] ?></p>
+          <div class="card-buttons">
+            <button id="btn-more-info" class="btn btn-primary">Detalles</button>
+            <button id="btn-edit" class="btn btn-success">Editar</button>
+            <a href="delete.php?id_user=<?php echo $row['id']?>" id="btn-delete" style="display: flex; align-items: center;" class="btn btn-danger">Eliminar</a>
+          </div>
+        </div>
+      </div>
+
+      <?php
+        }
+      ?>
+      <!-- <div class="my-card">
         <img class="card-img" src="../assets/placeholder_1.jpg" alt="Image">
         <div class="card-info">
           <p class="card-title">Nombre del Restaurante</p>
@@ -75,21 +104,11 @@
             <button id="btn-delete" class="btn btn-danger">Eliminar</button>
           </div>
         </div>
-      </div>
-      <div class="my-card">
-        <img class="card-img" src="../assets/placeholder_1.jpg" alt="Image">
-        <div class="card-info">
-          <p class="card-title">Nombre del Restaurante</p>
-          <p class="card-desc">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequuntur nam illum perspiciatis magni sunt quo, assumenda ducimus obcaecati? Vero, a soluta? Itaque fugiat minus cum exercitationem laboriosam, quae ipsum repellat?</p>
-          <div class="card-buttons">
-            <button id="btn-more-info" class="btn btn-primary">Detalles</button>
-            <button id="btn-edit" class="btn btn-success">Editar</button>
-            <button id="btn-delete" class="btn btn-danger">Eliminar</button>
-          </div>
-        </div>
-      </div>
+      </div> -->
+
     </div>
   </div>
+
   <template id="catalog-template">
     <div class="my-card">
       <img class="card-img" src="../assets/placeholder_1.jpg" alt="Image">
---
+++ utils/delete.php
@@ -0,0 +1,21 @@
+<?php
+    include "connection.php";
+    $obj =  new connection();
+    $conn = $obj->connection_db();
+
+    $id_user = $_GET["id_user"];
+
+    $sql =  "DELETE FROM restaurantes where id=".$id_user;
+
+    $delete = false;
+
+    try {
+        $conn->query($sql);
+        $delete = true;
+    } catch (\Throwable $th) {
+        $delete = false;
+    }
+    header("location: index.php");
+
+
+?>
